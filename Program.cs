874823280a37bf3a74404231892bbdb8ba867ecf
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-07 21:48:45 -0300

    resolução

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---\n");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("O objeto está se movendo (S/N)? ");
+Console.ResetColor();
+
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Deveria? ");
+Console.ResetColor();
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+
+string mensagem;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
+
+Console.ResetColor();
